5c9fc7e230322bee77e22582875266017c0e13e7
sashabelooov <beloovsasha23@gmail.com>
2025-11-13 21:38:34 +0500

    changed entrypoin file

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -14,15 +14,16 @@ python manage.py shell <<'PY'
 from decouple import config
 from django.contrib.auth import get_user_model
 User = get_user_model()
-email = config('DJANGO_SUPERUSER_EMAIL', default=None)
+username = config('DJANGO_SUPERUSER_USERNAME', default=None)
 pwd   = config('DJANGO_SUPERUSER_PASSWORD', default=None)
-if email and pwd and not User.objects.filter(email=email).exists():
-    print(f"Creating superuser {email}...")
-    User.objects.create_superuser(email=email, password=pwd)
+if username and pwd and not User.objects.filter(username=username).exists():
+    print(f"Creating superuser {username}...")
+    User.objects.create_superuser(username=username, password=pwd)
     print("Superuser created.")
 else:
     print("Superuser exists or env not provided; skipping.")
 PY
+
 
 echo "Starting Gunicorn..."
 gunicorn core.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --timeout 120
